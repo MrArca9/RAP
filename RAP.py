c5539d6318998994dbb6b17d0140965582ee79e8
MrArca9 <9Arca9@gmail.com>
2018-12-18 03:26:04 -0500

    Add files via upload
    Added tags to database generation.

--- RAP.py
+++ RAP.py
@@ -1,11 +1,11 @@
-# -*- coding: latin-1 -*-
-
 import requests
 import re
 from re import RegexFlag
 import time
 import datetime
 import json
+import urllib
+import html
 from html.parser import HTMLParser
 ParsedAnime =[]
 from urllib.parse import unquote
@@ -37,9 +37,11 @@ class Show(object):
         self.Rating = 0
         self.EndDate = ""
         self.ImageUrl = ""
+        self.Genres = []
 
 class Seasons(object):
     def __init__(self):
+        self.Version = str(datetime.datetime.now().timestamp())
         self.Winter = []
         self.Spring = []
         self.Summer = []
@@ -53,52 +55,6 @@ class Seasons(object):
     def addShowToFall(self, Show):
         self.Fall.append(Show)
 
-#winterString = ""
-#springString = ""
-#summerString = ""
-#fallString = ""
-#with open('winter Anime.txt', 'r') as myfile:
-#    winterString=myfile.read()
-#with open('spring Anime.txt', 'r') as myfile:
-#    springString=myfile.read()
-#with open('summer Anime.txt', 'r') as myfile:
-#    summerString=myfile.read()
-#with open('fall Anime.txt', 'r') as myfile:
-#    fallString=myfile.read()
-#GlobalString = winterString + "\n SPRINGSHOWSSTARTHERE \n" + springString + "\n SUMMERSHOWSSTARTHERE \n" + summerString + "\n FALLSHOWSSTARTHERE \n" + fallString
-#for showString in GlobalString.splitlines():
-#    showString = showString.strip()
-#    if(showString == '\n'):
-#        continue
-#    if showString.strip() == "":
-#        continue
-#    if showString == '9+:':
-#        continue
-#    if showString == '8:':
-#        continue
-#    if showString == '7:':
-#       continue
-#    if showString == '6:':
-#       continue
-#    if showString == '5:':
-#        continue
-#    if showString == '4:':
-#       continue
-#    if showString == '3:':
-#        continue
-#    if showString == '2:':
-#        continue
-#    if showString == '1:':
-#        continue
-#    if showString == 'SPRINGSHOWSSTARTHERE':
-#        continue
-#    if showString == 'SUMMERSHOWSSTARTHERE':
-#        continue
-#    if showString == 'FALLSHOWSSTARTHERE':
-#        continue
-#    showString
-#    #ParsedAnime.append(showString.split(")")[1][1:])
-#print(ParsedAnime)
 i = 1
 AnimeSeason = Seasons()
 while(i < 5):
@@ -131,36 +87,51 @@ while(i < 5):
     Anime23Sorted = []
     Anime12 = {}
     Anime12Sorted = []
-    response = requests.get(url)
-    taggedList = re.findall('<div class=\"seasonal-anime js-seasonal-anime\".*?</div>', str(response.content))
+    response0 = requests.get(url).content
+    response = str(response0, 'utf-8', errors='replace')
+    taggedList = re.findall('<div class=\"seasonal-anime js-seasonal-anime\".*?</div>', response, flags= RegexFlag.DOTALL)
     for tag in taggedList:
         AnimeName = cleanUp(re.findall('link-title\">.*?</a>', tag)[0].replace('link-title">','').replace('</a>', ''))
         AnimeUrl = cleanUp(re.findall('<a href=".*? ', tag)[0].replace('<a href="','').replace('/video"',''))
-        if( 'Uchuu Senkan Tiramis' in AnimeName):
-            with open("AnimeDatabase.txt", "w+") as text_file:
-                text_file.write(AnimeName)
         if(AnimeName in ParsedAnime):
             print('***SKIPPING PARSED ANIME '+AnimeName+'***')
             continue
         time.sleep(4)
-        response = str(requests.get(AnimeUrl).content)
-        Aired = re.findall('<span class="dark_text">Aired:</span>.*?</div>',response)[0].replace('\\n','').replace('<span class="dark_text">Aired:</span>','').replace('</div>','')
+        response = str(requests.get(AnimeUrl).content, 'utf-8', errors='replace')
+        Aired = re.findall('<span class="dark_text">Aired:</span>.*?</div>',response, flags=RegexFlag.DOTALL)[0].replace('\\n','').replace('<span class="dark_text">Aired:</span>','').replace('</div>','')
         try:
-            Rating = re.findall('<span itemprop="ratingValue">.*?</span>',response)[0].replace('\\n','').replace('<span itemprop="ratingValue">','').replace('</span>','')
+            Rating = re.findall('<span itemprop="ratingValue">.*?</span>',response, flags=RegexFlag.DOTALL)[0].replace('\\n','').replace('<span itemprop="ratingValue">','').replace('</span>','')
         except:
             Rating = re.findall('<div class="po-r js-statistics-info di-ib" data-id="info1">.*?</div>',response, flags=RegexFlag.DOTALL)[0]
-            Rating = re.findall('<span>.*?</span>', Rating)[0].replace('<span>','').replace('</span>','')
+            Rating = re.findall('<span>.*?</span>', Rating, flags=RegexFlag.DOTALL)[0].replace('<span>','').replace('</span>','')
         try:
             reImageString = '<img src="https://myanimelist.cdn-dena.com/images/anime/.*?itemprop="image"'
             ImageUrl = re.findall(reImageString, response, flags=RegexFlag.DOTALL)[0].split("<img src=\"")[1].split('"')[0]
         except:
-            print("ERROR GETTING IMAGE URL")
-            ImageUrl = ""
+            try:
+                reImageString = '<img src="https://cdn.myanimelist.net/images/anime/.*?itemprop="image"'
+                ImageUrl = re.findall(reImageString, response, flags=RegexFlag.DOTALL)[0].split("<img src=\"")[1].split('"')[0]
+            except:
+                print("ERROR GETTING IMAGE URL")
+                ImageUrl = ""
         try:
             desc = strip_tags(re.findall('<span itemprop="description">.*?</span>', response, flags=RegexFlag.DOTALL)[0].replace('<span itemprop="description">','').replace('</span>','')).replace("\\n",'').replace('\\r','').replace('[Written by MAL Rewrite]','')
         except:
             print("ERROR GETTING DESC")
             desc = "None"
+        try:
+            genresCompressed =re.findall('<span class=\"dark_text\">Genres.*?</div>', response, flags=RegexFlag.DOTALL)[0]
+            animeType = re.findall('>.*?<', re.findall('<span class=\"dark_text\">Type:.*?</div>', response, flags=RegexFlag.DOTALL)[0], flags=RegexFlag.DOTALL)[2].replace('>','').replace('<','')
+            genreList = re.findall('title=.*?\">', genresCompressed, flags=RegexFlag.DOTALL)
+            genreListClean = []
+            genreListClean.append(animeType)
+            for stringValue in genreList:
+                stringValue = stringValue.replace('title="','').replace('">','')
+                genreListClean.append(stringValue)
+            
+        except:
+            print("ERROR GETTING GENRES")
+            genreListClean = []
         if('N/A' not in Rating):
             Rating = float(Rating)
         else:
@@ -185,6 +156,7 @@ while(i < 5):
             show.EndDate = EndAired
             show.ImageUrl = ImageUrl
             show.Desc = desc
+            show.Genres = genreListClean
             if(Rating > 9):
                 Anime9Plus[AnimeName] = DT_EndAired
                 show.Rating = 10
@@ -222,6 +194,8 @@ while(i < 5):
                 AnimeSeason.addShowToSummer(show.__dict__)
             if season == "fall":
                 AnimeSeason.addShowToFall(show.__dict__)
+            with open("AnimeDatabase.json", "w+") as text_file:
+                text_file.write(json.dumps(AnimeSeason.__dict__))
         else:
             print(AnimeName+' Has not aired yet. ('+Aired+')')
             ParsedAnime.append(AnimeName)
